14a6bf9b3e5679dbcd8e69a11d1f1745f99086d0
Aleblanco25 <ale.blanco.2589@gmail.com>
2022-11-26 11:48:51 -0600

    entegable2 sin terminar

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import './App.css'
+import Loading from './components/Loading'
+import WeatherCard from './components/WeatherCard'
+
+
+function App() {
+
+
+  const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+  const [temp, setTemp] = useState()
+
+  const success = pos => {
+    setCoords({
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+})
+}
+
+useEffect (() =>{
+
+  navigator.geolocation.getCurrentPosition(success)
+
+}, [])
+
+useEffect (()=>{
+
+  if(coords){
+  const apiKey = `4c727104b33491b3d662a6438bc2bae1`
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}`
+  axios.get(URL)
+    .then(res => { setWeather(res.data)
+      const celsius = (res.data.main.temp -273).toFixed(1);
+      const farenheit = (celsius * (9/5) + 32).toFixed(1)
+      setTemp ({celsius, farenheit})
+    })
+    .catch(err => console.log(err))
+  }
+
+},[coords])
+
+ 
+
+  return (
+    <div className="App">
+      { weather ?
+      <WeatherCard 
+      weather={weather}
+      temp={temp}
+      />: <Loading/>}
+    </div>
+  )
+}
+
+
+export default App
+
+
+//&units=metric
